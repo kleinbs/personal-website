4b0a738147198acdc112f8cb8ede1ad96e19f3e1
kleinbs <kleinbs1234@gmail.com>
2018-09-23 19:09:01 -0700

    made some changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: './src/js/index.js',
@@ -30,10 +31,20 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebPackPlugin(),
+        new HtmlWebPackPlugin({
+            filename: 'index.html',
+            template: 'src/html/index.html'
+        }),
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
-        })
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: 'src/assets/*',
+                to: './',
+                flatten: true
+            }
+        ])
     ]
 };
